f1d6c694522863144dcd4719ff51e71ef3307a8b
Rahul Duggal <duggalr42@gmail.com>
2022-11-11 11:47:51 -0500

    refactoring

--- creator_coin_new/settings.py
+++ creator_coin_new/settings.py
@@ -268,3 +268,15 @@ else:
 
 
 
+if 'RDS_DB_NAME' in os.environ:
+    CSRF_COOKIE_SECURE = True
+    CSRF_COOKIE_SAMESITE = 'Strict'
+    SESSION_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_SSL_REDIRECT = True
+    X_FRAME_OPTIONS = 'DENY'
+    
+
+
+
